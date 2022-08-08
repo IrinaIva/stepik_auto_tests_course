a0c98a9e89c70ad7cd6451f6f495cf6897974cff
Irina Popova <irina.popova.gdc@fujitsu.com>
2022-08-08 21:36:22 +0300

    extra

--- test_items.py
+++ test_items.py
@@ -1,12 +1,6 @@
-import pytest
-from selenium import webdriver
-from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
-import time
-import math
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
 
 url = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
 
