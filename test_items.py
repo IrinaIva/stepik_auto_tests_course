02f700c4ac4881a945bf207150fc2d5281ab2f69
Irina Popova <irina.popova.gdc@fujitsu.com>
2022-08-08 21:01:31 +0300

    3.6 Задание: запуск автотестов для разных языков интерфейса

--- test_items.py
+++ test_items.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support.ui import Select
 
 url = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
 
-def test_see_add_button(browser):
+def test_check_add_button(browser):
     browser.get(url)
     WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//form[@id='add_to_basket_form']//button")))
 
